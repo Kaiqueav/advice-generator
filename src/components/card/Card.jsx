83260f8cf625e3e302f012e221443f6f98b0a333
Kaiqueav <kaiquecerq66@outlook.com>
2024-01-10 16:39:16 -0300

    fazendo consumo da api

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,13 +1,28 @@
-import DivMobile from './assets/images/pattern-divider-mobile-desktop.svg'
+import icon from '/img/icon-dice.svg';
+import dividerDesktop from '/img/pattern-divider-desktop.svg'
+import dividerMobile from '/img/pattern-divider-mobile.svg'
 
-
+import { useState, useEffect } from 'react';
+import axios from 'axios';        
 export const Card = () => {
+         const [advice,  setAdvice] = useState("")
+         const  getAdvice = async() =>{
+            const response = await axios.get("https://api.adviceslip.com/advice");
+            const advice = await response.data.slip;
+            setAdvice(advice)
+            }
+            useEffect(() =>{
+              getAdvice()
+         }, [])
     return (
         <div className="card">
-            <p> ADVICE #1</p>
-            <h2> esse é um conselho</h2>
-            <img src={DivMobile} alt=""   />
-             
+            <p> ADVICE #{advice.id}</p>
+            <h2>"{advice.advice}"</h2>
+            <img src={dividerDesktop} className="divider-desktop" alt="divider"  />
+            <img src={dividerMobile} className="divider-mobile" alt="divider" />
+            <div className="dice"  >
+                <img src={icon} alt="dice"/>
+            </div>
         </div>
     )
 
