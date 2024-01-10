557062ff12fbccf16bcae92d6306ddbcd4184ea6
Kaiqueav <kaiquecerq66@outlook.com>
2024-01-10 14:07:34 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Card } from "./components/card/Card"
+
+function App() {
+   
+
+  return (
+    <div className="bg-slate-900">
+      <Card/>
+    </div>
+       
+  )
+}
+
+export default App
---
+++ src/components/card/Card.jsx
@@ -0,0 +1,10 @@
+export const Card = () => {
+    return (
+        <div className=" m-6 flex justify-center items-center bg-red-400 w-[359px] h-[350px] p-5">
+             
+                <h3 className="text-green-500 text-center" >ADVICE #117</h3>
+             
+        </div>
+    )
+
+}
